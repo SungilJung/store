69e3cefefcb1ffbd0eebefd8ba1c5dd5b616d5c7
SungilJung <green5940@gmail.com>
2021-12-15 17:49:31 +0900

    테스트를 위해 앱바에 현재 location 추가.

--- store/lib/src/adapter/presentation/modules/root/root_view.dart
+++ store/lib/src/adapter/presentation/modules/root/root_view.dart
@@ -9,9 +9,14 @@ class RootView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetRouterOutlet.builder(
       builder: (context, delegate, current) {
+        final title = current?.location;
+
         return Scaffold(
+          appBar: AppBar(
+            title: Text(title ?? ''),
+            centerTitle: true,
+          ),
           body: GetRouterOutlet(
-            anchorRoute: Routes.storeRoot,
             initialRoute: Routes.storeRoot,
           ),
         );
