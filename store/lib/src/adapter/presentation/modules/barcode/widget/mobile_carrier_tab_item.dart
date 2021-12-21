594afd25fe4f27560e30c2154abca44fc35ed20a
SungilJung <green5940@gmail.com>
2021-12-21 18:21:30 +0900

    바코드 입력 페이지 구현 중.

---
+++ store/lib/src/adapter/presentation/modules/barcode/widget/mobile_carrier_tab_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../model/mobile_carrier.dart';
+
+class MobileCarrierTabItem extends StatelessWidget {
+  final MobileCarrier carrier;
+  final double width;
+  final double height;
+  final Color color;
+  final ValueChanged<int>? onTap;
+  final TextStyle textStyle;
+
+  const MobileCarrierTabItem({
+    Key? key,
+    required this.carrier,
+    required this.width,
+    this.height = 30,
+    required this.color,
+    required this.onTap,
+    required this.textStyle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap!(carrier.index);
+        }
+        ;
+      },
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1.5,
+            color: color,
+          ),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Center(
+          child: Text(
+            carrier.name,
+            style: textStyle.copyWith(color: color),
+          ),
+        ),
+      ),
+    );
+  }
+}
