6dd9c3f28ed7ee95c89634e889a1928c7a297931
SungilJung <green5940@gmail.com>
2021-12-21 18:13:15 +0900

    메시지 외부화 기초 작업 진행

--- store/lib/src/common/message/messages.dart
+++ store/lib/src/common/message/messages.dart
@@ -5,4 +5,7 @@ abstract class Messages {
   static const String pbProduct = 'PB 상품';
   static const String map = '지도';
   static const String favoriteProduct = '찜한 상품';
+  static const String membershipTitle = '나의 멤버십';
+  static const String membership = '멤버십';
+  static const String inputMembershipCode = '멤버십 번호를 입력하세요';
 }
